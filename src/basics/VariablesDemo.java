587512246ce114d5d44d21cd3e4d3740cbb69034
sanidhy-main <sanidhy.25bcon1241@jecrcu.edu.in>
2026-01-10 11:19:57 +0400

    Fixed folder issues, and completed VariablesDemo

---
+++ src/basics/VariablesDemo.java
@@ -0,0 +1,12 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+        int age = 18;
+        double height = 175.5;
+        boolean isStudent = true; //true is always lowercase
+        char grade = 'A';
+        String name = "Sanidhy";
+
+        System.out.println(age);
+        System.out.println(name);
+    }
+}
